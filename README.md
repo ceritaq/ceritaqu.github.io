707ad06c4404ae986bc76798ca8b711e35192417
alfawwaz11 <92318792+alfawwaz11@users.noreply.github.com>
2021-10-11 21:31:55 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# duniaceritaq
+# duniaceritaq
---
+++ proses.php
@@ -0,0 +1,9 @@
+<?php
+ $message = $_POST['message'];
+ $name = $_POST['name'];
+ 
+
+echo "Selamat $nama , anda berhasil membuat form sederhana<br><br>";
+echo "nama = $nama <br>umur = $umur <br>alamat = $alamat";
+
+ ?>
